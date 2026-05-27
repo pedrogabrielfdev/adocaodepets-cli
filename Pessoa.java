8d3f28c8332daea3f52fccbbb111b6a2f3d55ca5
Caio Cordeiro <caiocordeirodossantoss@gmail.com>
2026-05-26 23:11:53 -0300

    Adicionando os getters e setters

--- Pessoa.java
+++ Pessoa.java
@@ -1,17 +1,53 @@
 public class Pessoa {
-    private String nome, sexo, senha, endereço, telefone, cpf;
+    private String nome, sexo, senha, endereco, telefone, cpf;
     private int idade, id;
 
-    Pessoa(String nome, String sexo, String senha, String endereço, String telefone, String cpf, int idade, int id) {
-
+    Pessoa(String nome, String sexo, String senha, String endereco, String telefone, String cpf, int idade, int id) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.senha = senha;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.id = id;
     }
 
-    public void set() {
-
+    public String getnome() {
+        return this.nome;
     }
 
-    public void get() {
+    public void setnome(String nome) {
+        this.nome = nome;
+    }
 
+    public String getendereco() {
+        return this.endereco;
+    }
+
+    public void setendereco(String novoEndereco) {
+        this.endereco = novoEndereco;
+    }
+
+    public String gettelefone() {
+        return this.telefone;
+    }
+
+    public void settelefone(String novoTelefone) {
+        this.endereco = novoTelefone;
+    }
+    
+    public int getidade() {
+        return this.idade;
+    }
+
+    public void setidade(int idade) {
+        if (idade >= 18) {
+            this.idade = idade;
+            System.out.println("Idade Validada! Prossiga com o cadastro!");
+        } else {
+            System.out.println("Idade Inválida! Você não tem idade suficiente para adotar um pet.");
+        }
     }
 
     public void editar(String novoEndereco, String novoTelefone, String novaSenha) {
--- Pets.java
+++ Pets.java
@@ -5,24 +5,76 @@ public class Pets {
     private boolean adotado, vacinado;
 
     Pets(int idade, int id, String especie, String raca, String sexo, String nome, String dataNascimento, String cor, String comorbidade, double tamanho, double peso, boolean adotado, boolean vacinado) {
-
+        this.idade = idade;
+        this.id = id;
+        this.especie = especie;
+        this.raca = raca;
+        this.sexo = sexo;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.cor = cor;
+        this.comorbidade = comorbidade;
+        this.tamanho = tamanho;
+        this.peso = peso;
+        this.adotado = adotado;
+        this.vacinado = vacinado;
     }
 
-    public void set() {
-
+    public int getidade() {
+        return this.idade;
     }
 
-    public void get() {
+    public void setidade(int idade) {
+        this.idade = idade;
+    }
 
+    public String getnome() {
+        return this.nome;
+    }
+
+    public void setnome(String novoNome) {
+        this.nome = novoNome;
+    }
+
+    public String getcomorbidade() {
+        return this.comorbidade;
+    }
+
+    public void setcomorbidade(String novoEstado) {
+        this.comorbidade = novoEstado;
+    }
+
+    public double gettamanho() {
+        return this.tamanho;
+    }
+
+    public void settamanho(double novoTamanho) {
+        this.tamanho = novoTamanho;
+    }
+
+    public double getpeso() {
+        return this.peso;
+    }
+
+    public void setpeso(double novoPeso) {
+        this.peso = novoPeso;
+    }
+
+    public boolean getadotado() {
+        return this.adotado;
+    }
+
+    public void setadotado(boolean adotado) {
+        this.adotado = adotado;
     }
 
     public void cadastrarPets() {
 
     }
 
-    public void mudarNome(String novoNome) {
+    // public void mudarNome(String novoNome) {
         
-    }
+    // }
 
     public void exibirInformações() {
 
